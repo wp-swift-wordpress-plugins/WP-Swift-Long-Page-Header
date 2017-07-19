22d51d0c7a0e7abecfb9b5a086da8423d8faa129
Gary Swift <garyswiftmail@gmail.com>
2017-07-19 12:22:08 +0100

    Format changes

--- index.php
+++ index.php
@@ -20,49 +20,53 @@ function wp_swift_long_page_title( $title ) {
 	return $title;
 }
 add_filter( 'the_title', 'wp_swift_long_page_title' );
-if( function_exists('acf_add_local_field_group') ):
 
-acf_add_local_field_group(array (
-	'key' => 'group_596b4867bf454',
-	'title' => 'Long Header',
-	'fields' => array (
-		array (
-			'key' => 'field_596b486d20402',
-			'label' => 'Long Header',
-			'name' => 'long_header',
-			'type' => 'text',
-			'instructions' => 'Optional descriptive header which will be used as the h1 header',
-			'required' => 0,
-			'conditional_logic' => 0,
-			'wrapper' => array (
-				'width' => '',
-				'class' => '',
-				'id' => '',
-			),
-			'default_value' => '',
-			'placeholder' => '',
-			'prepend' => '',
-			'append' => '',
-			'maxlength' => '',
-		),
-	),
-	'location' => array (
-		array (
+function wp_swift_acf_field_group_long_header() {
+	if( function_exists('acf_add_local_field_group') ):
+
+	acf_add_local_field_group(array (
+		'key' => 'group_596b4867bf454',
+		'title' => 'Long Header',
+		'fields' => array (
 			array (
-				'param' => 'post_type',
-				'operator' => '==',
-				'value' => 'page',
+				'key' => 'field_596b486d20402',
+				'label' => 'Long Header',
+				'name' => 'long_header',
+				'type' => 'text',
+				'instructions' => 'Optional descriptive header which will be used as the h1 header',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array (
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
 			),
 		),
-	),
-	'menu_order' => 0,
-	'position' => 'acf_after_title',
-	'style' => 'seamless',
-	'label_placement' => 'top',
-	'instruction_placement' => 'label',
-	'hide_on_screen' => '',
-	'active' => 1,
-	'description' => 'Optional descriptive header which will be used as the h1 header',
-));
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'page',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'acf_after_title',
+		'style' => 'seamless',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => 1,
+		'description' => 'Optional descriptive header which will be used as the h1 header',
+	));
 
-endif;
+	endif;	
+}
+add_action( 'admin_init', 'wp_swift_acf_field_group_long_header' );
